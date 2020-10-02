396d593578dfe1f7e97ca88777fafed3911dc8c5
nsrau <new.urbanetz@gmail.com>
2020-10-02 23:57:14 +0200

    refactor: move sut creation to a factor method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,27 @@
 import { HttpPostClientSpy } from "../../test/mock-http-client"
 import { RemoteAuthentication } from "./remove-authentication"
 
+type SutTypes = {
+    sut: RemoteAuthentication
+    httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url = 'any_url'): SutTypes => {
+    const httpPostClientSpy = new HttpPostClientSpy()
+    // system under test
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+
+    return {
+        sut,
+        httpPostClientSpy
+    }
+}
+
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', async () => {
-        const url = 'any_url'
-        const httpPostClientSpy = new HttpPostClientSpy()
-        // system under test
-        const sut = new RemoteAuthentication(url, httpPostClientSpy)
+        const url = 'url'
+        
+        const { sut, httpPostClientSpy } = makeSut(url)
         await sut.auth()
         expect(httpPostClientSpy.url).toBe(url)
     })
