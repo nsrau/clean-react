e51109885ea5c4e60d3e3154660b20b421304fea
nsrau <new.urbanetz@gmail.com>
2020-10-03 00:02:08 +0200

    refactor: chenge RemoteAuthentication to use fakerer

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,13 @@
 import { HttpPostClientSpy } from "../../test/mock-http-client"
 import { RemoteAuthentication } from "./remove-authentication"
+import faker from 'faker'
 
 type SutTypes = {
     sut: RemoteAuthentication
     httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url = 'any_url'): SutTypes => {
+const makeSut = (url = faker.internet.url()): SutTypes => {
     const httpPostClientSpy = new HttpPostClientSpy()
     // system under test
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
@@ -19,8 +20,8 @@ const makeSut = (url = 'any_url'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', async () => {
-        const url = 'url'
-        
+        const url = faker.internet.url()
+
         const { sut, httpPostClientSpy } = makeSut(url)
         await sut.auth()
         expect(httpPostClientSpy.url).toBe(url)
