d6b392f2255e3201f818156da7206498039c2ad7
nsrau <new.urbanetz@gmail.com>
2020-10-02 23:52:59 +0200

    refactor: move HttpPostClientSpy

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,17 +1,8 @@
-import { HttpPostClient } from "../../protocols/http/http-post-client"
+import { HttpPostClientSpy } from "../../test/mock-http-client"
 import { RemoteAuthentication } from "./remove-authentication"
 
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', async () => {
-        class HttpPostClientSpy implements HttpPostClient {
-            url?: string
-            async post(url: string): Promise<void> {
-                this.url = url
-
-                return Promise.resolve()
-            }
-        }
-
         const url = 'any_url'
         const httpPostClientSpy = new HttpPostClientSpy()
         // system under test
