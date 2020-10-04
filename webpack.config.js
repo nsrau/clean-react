a6e6f5b9738a56e0db8eb9f4032cf99aa5e73657
nsrau <new.urbanetz@gmail.com>
2020-10-04 23:29:05 +0200

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -10,10 +10,29 @@ module.exports = {
         fileName: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
             '@': path.join(__dirname, 'src')
         }
+    },
+    modeule: {
+        rules: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader',
+                options: {
+                    modules: true
+                }
+            }, {
+                loader: 'sass-loader'
+            }]
+        }]
     },
     devServer: {
         contentBase: './public',
