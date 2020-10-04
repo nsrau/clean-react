c7d37eacb354441e0bc4875bb2d27aefa7ce470a
nsrau <new.urbanetz@gmail.com>
2020-10-05 00:13:22 +0200

    feat: init presentation login

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public/js'),
         publicPath: '/public/js',
-        fileName: 'bundle.js'
+        filename: 'bundle.js'
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', 'scss'],
@@ -15,7 +15,7 @@ module.exports = {
             '@': path.join(__dirname, 'src')
         }
     },
-    modeule: {
+    module: {
         rules: [{
             test: /\.ts(x?)$/,
             loader: 'ts-loader',
