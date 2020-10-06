12690f9af5923fe83d785850d06f6ee0d31b2576
nsrau <new.urbanetz@gmail.com>
2020-10-06 23:20:01 +0200

    test: ensure login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,7 +7,17 @@ describe('Login Component', () => {
         const { getByTestId } = render(<Login />)
         const errorWrap = getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
+        
         const submitButton = getByTestId('submit') as HTMLButtonElement
         expect(submitButton.disabled).toBe(true)
+    
+        const emailStatus = getByTestId('email-status')   
+        expect(emailStatus.title).toBe('field required')
+        expect(emailStatus.textContent).toBe('🔴')
+
+        const passwordStatus = getByTestId('password-status')   
+        expect(passwordStatus.title).toBe('field required')
+        expect(passwordStatus.textContent).toBe('🔴')
+
     })
 })
