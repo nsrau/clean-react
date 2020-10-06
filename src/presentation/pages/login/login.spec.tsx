eb634b81bf8ef204631f3b59ba7831496dd00bd3
nsrau <new.urbanetz@gmail.com>
2020-10-06 23:58:45 +0200

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -49,12 +49,21 @@ describe('Login Component', () => {
 
     })
 
-    test('Should call Validationn with correct values', () => {
+    test('Should call Validationn with correct email', () => {
         const { sut, validationSpy } = makeSut()
         const emailInput = sut.getByTestId('email')
         fireEvent.input(emailInput, { target: { value: 'any_email' } })
         expect(validationSpy.input).toEqual({
             email: 'any_email'
+        })
+    })
+
+    test('Should call Validationn with correct password', () => {
+        const { sut, validationSpy } = makeSut()
+        const passwordInput = sut.getByTestId('password')
+        fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+        expect(validationSpy.input).toEqual({
+            password: 'any_password'
         })
     })
 })
