945846a1440583a37b8d128806c5697c0388fe69
nsrau <new.urbanetz@gmail.com>
2020-10-07 23:42:47 +0200

    test: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -78,4 +78,15 @@ describe('Login Component', () => {
         expect(passwordStatus.title).toBe('ok')
         expect(passwordStatus.textContent).toBe('🟢')
     })
+
+    test('Should enable submit button if form is valid', () => {
+        const { sut, validationStub } = makeSut()
+        validationStub.errorMessage = null
+        const emailInput = sut.getByTestId('email')
+        fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+        const passwordInput = sut.getByTestId('password')
+        fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+        const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+        expect(submitButton.disabled).toBe(false)
+    })
 })
