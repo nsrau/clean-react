312b7db0b74da74da059751c02cb9fe03e0041dd
nsrau <new.urbanetz@gmail.com>
2020-10-07 23:12:26 +0200

    test: ensure Login present error if email is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -11,6 +11,7 @@ type SutTypes = {
 
 const makeSut = (): SutTypes => {
     const validationSpy = new ValidationSpy()
+    validationSpy.errorMessage = faker.random.words()
     const sut = render(<Login validation={validationSpy} />)
 
     return {
@@ -23,7 +24,7 @@ describe('Login Component', () => {
     afterEach(cleanup)
 
     test('Should start with initial state', () => {
-        const { sut } = makeSut()
+        const { sut, validationSpy } = makeSut()
         const errorWrap = sut.getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
 
@@ -31,7 +32,7 @@ describe('Login Component', () => {
         expect(submitButton.disabled).toBe(true)
 
         const emailStatus = sut.getByTestId('email-status')
-        expect(emailStatus.title).toBe('field required')
+        expect(emailStatus.title).toBe(validationSpy.errorMessage)
         expect(emailStatus.textContent).toBe('🔴')
 
         const passwordStatus = sut.getByTestId('password-status')
@@ -56,5 +57,14 @@ describe('Login Component', () => {
         fireEvent.input(passwordInput, { target: { value: password } })
         expect(validationSpy.fieldName).toBe('password')
         expect(validationSpy.fieldVale).toBe(password)
+    })
+
+    test('Should show email error if Validationn fails', () => {
+        const { sut, validationSpy } = makeSut()
+        const emailInput = sut.getByTestId('email')
+        fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+        const emailStatus = sut.getByTestId('email-status')
+        expect(emailStatus.title).toBe(validationSpy.errorMessage)
+        expect(emailStatus.textContent).toBe('🔴')
     })
 })
