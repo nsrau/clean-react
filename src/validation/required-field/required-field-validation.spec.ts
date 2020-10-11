21ffe269a7284e81a5b2850ecf76b7f95614a314
nsrau <new.urbanetz@gmail.com>
2020-10-11 22:13:25 +0200

    test: ensure RequiredFieldValitaion returns error if field is empty

---
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -0,0 +1,16 @@
+import { RequiredFieldError } from "@/validation/errors"
+import { RequiredFieldValidation } from "./required-field-validation"
+
+describe('RequiredFieldValidation', () => {
+    test('Should return error if field is empty', () => {
+        const sut = new RequiredFieldValidation('email')
+        const error = sut.validate('')
+        expect(error).toEqual(new RequiredFieldError())
+    })
+
+    test('Should return error if field is empty', () => {
+        const sut = new RequiredFieldValidation('email')
+        const error = sut.validate('')
+        expect(error).toEqual(new RequiredFieldError())
+    })
+})
