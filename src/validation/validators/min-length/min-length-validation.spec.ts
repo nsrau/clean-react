6c60341707d0dd63ef63fea85a334f86362156e6
nsrau <new.urbanetz@gmail.com>
2020-10-11 22:59:41 +0200

    test: ensure MinLength returns falsy if values is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,10 +1,18 @@
 import { InvalidFieldError } from '@/validation/errors/invalid-field-error';
 import { MinLengthValidation } from './min-length-validation';
 
+
+
 describe('MinLengthValidation', () => {
     test('Should return error if value is invalid', () => {
         const sut = new MinLengthValidation('field', 5)
         const error = sut.validate('124')
         expect(error).toEqual(new InvalidFieldError())
+    })
+
+    test('Should return falsy if value is valid', () => {
+        const sut = new MinLengthValidation('field', 5)
+        const error = sut.validate('12345')
+        expect(error).toBeFalsy()
     })
 })
