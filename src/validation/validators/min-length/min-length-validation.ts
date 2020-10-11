3dcb5b8fc486de5c905f763524e3861c5158eb5b
nsrau <new.urbanetz@gmail.com>
2020-10-11 22:54:50 +0200

    feat: ensure MinLengthValdiaiton return error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,9 @@
+import { InvalidFieldError } from '@/validation/errors/invalid-field-error';
+import { FieldValidation } from '@/validation/protocols/field-validation';
+
+export class MinLengthValidation implements FieldValidation {
+    constructor(readonly field: string, private readonly minLength: number) { }
+    validate(value: string): Error {
+        return new InvalidFieldError()
+    }
+}
