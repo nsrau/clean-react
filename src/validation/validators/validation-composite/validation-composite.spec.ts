38ef4c12d4e52ecf59ecd419625f92e591e7912f
nsrau <new.urbanetz@gmail.com>
2020-10-11 23:26:54 +0200

    test: ensure Validationcomposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,6 +4,7 @@ import { ValidationComposite } from './validation-composite';
 describe('ValidationComposite', () => {
     test('Should return error if any validaiton fails', () => {
         const fieldValidationSpy = new FieldValidationSpy('any_field')
+        fieldValidationSpy.error = new Error('first_error_message')
         const fieldValidationSpy2 = new FieldValidationSpy('any_field')
         fieldValidationSpy2.error = new Error('any_error_message')
         const sut = new ValidationComposite([
@@ -11,6 +12,6 @@ describe('ValidationComposite', () => {
             fieldValidationSpy2
         ])
         const error = sut.validate('any_field', 'any_value')
-        expect(error).toBe('any_error_message')
+        expect(error).toBe('first_error_message')
     })
 })
