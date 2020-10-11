c37fd8801853c46cee0a040cf3f054847c4bf3d2
nsrau <new.urbanetz@gmail.com>
2020-10-11 22:37:40 +0200

    test: ensure EmailValidation returns error if email is invalid

---
+++ src/validation/validators/email/email-validation.spec.ts
@@ -0,0 +1,16 @@
+import { InvalidFieldError } from '@/validation/errors';
+import { EmailValidation } from './email-validation';
+
+describe('EmailValidation', () => {
+    test('Should return error if email is invalid', () => {
+        const sut = new EmailValidation('email')
+        const error = sut.validate('')
+        expect(error).toEqual(new InvalidFieldError())
+    })
+
+    test('Should return falsy if email is invalid', () => {
+        const sut = new EmailValidation('email')
+        const error = sut.validate('')
+        expect(error).toEqual(new InvalidFieldError())
+    })
+})
