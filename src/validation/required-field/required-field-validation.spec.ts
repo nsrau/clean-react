a88a4a450eddd19560d58c4e2b77a1f2c09e6c5e
nsrau <new.urbanetz@gmail.com>
2020-10-11 22:16:05 +0200

    test: ensure RequiredValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldError } from "@/validation/errors"
 import { RequiredFieldValidation } from "./required-field-validation"
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
     test('Should return error if field is empty', () => {
@@ -8,9 +9,9 @@ describe('RequiredFieldValidation', () => {
         expect(error).toEqual(new RequiredFieldError())
     })
 
-    test('Should return error if field is empty', () => {
+    test('Should return false if field is not empty', () => {
         const sut = new RequiredFieldValidation('email')
-        const error = sut.validate('')
-        expect(error).toEqual(new RequiredFieldError())
+        const error = sut.validate(faker.random.word())
+        expect(error).toBeFalsy()
     })
 })
