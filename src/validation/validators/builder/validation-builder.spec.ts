683d928bdf61616c7100f564aefaa32265e3ac5c
nsrau <new.urbanetz@gmail.com>
2020-10-12 22:23:32 +0200

    feat: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldValidation } from '@/validation/validators';
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators';
 import { ValidationBuilder as sut } from './validation-builder';
 
 
@@ -8,8 +8,8 @@ describe('ValidationBuilder', () => {
         expect(validations).toEqual([new RequiredFieldValidation('any_field')])
     })
 
-    test('Should return RequiredFieldValidation', () => {
-        const validations = sut.field('any_field').required().build()
-        expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    test('Should return EmailValidation', () => {
+        const validations = sut.field('any_field').email().build()
+        expect(validations).toEqual([new EmailValidation('any_field')])
     })
 })
