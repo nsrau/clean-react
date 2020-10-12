df549a23bb3730c6acbbd2442ffa689f215a4b29
nsrau <new.urbanetz@gmail.com>
2020-10-12 22:19:51 +0200

    test: ensure validation builder has a required helper

---
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -0,0 +1,15 @@
+import { RequiredFieldValidation } from '@/validation/validators';
+import { ValidationBuilder as sut } from './validation-builder';
+
+
+describe('ValidationBuilder', () => {
+    test('Should return RequiredFieldValidation', () => {
+        const validations = sut.field('any_field').required().build()
+        expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    })
+
+    test('Should return RequiredFieldValidation', () => {
+        const validations = sut.field('any_field').required().build()
+        expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    })
+})
