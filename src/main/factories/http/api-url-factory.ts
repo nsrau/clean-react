b52963be15a73f00095cca568c80ad5b39fbcded
nsrau <new.urbanetz@gmail.com>
2020-10-14 23:02:20 +0200

    feat: add env var to api url

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,5 +1,5 @@
 const makeApiUrl= (path: string): string => {
-    return `http://fordevs.herokuapp.com/api${path}`
+    return `process.env.API_URL${path}`
 }
 
 export default makeApiUrl
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { DefinePlugin } = require('webpack')
 
 module.exports = {
     mode: 'development',
@@ -44,6 +45,9 @@ module.exports = {
         'react-dom': 'ReactDOM'
     },
     plugins: [
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new DefinePlugin({
+            'process.env.API_URL': 'http://fordevs.herokuapp.com/api'
+        })
     ]
 }
