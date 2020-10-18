ad6413d1b1b2701b9cf79c0f82c7244c0a359183
nsrau <new.urbanetz@gmail.com>
2020-10-18 22:36:44 +0200

    fix: copy paste

--- src/infra/cache/local-storage-adpter.spec.ts
+++ src/infra/cache/local-storage-adpter.spec.ts
@@ -16,12 +16,4 @@ describe('LocalStorageAdapter', () => {
         await sut.set(key, value)
         expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
     })
-
-    test('Should call localStorage with correct values', async () => {
-        const sut = makeSut()
-        const key = faker.database.column()
-        const value = faker.random.word()
-        await sut.set(key, value)
-        expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
-    })
 })
