c6f07fc62c98cbbe3230599bd94bf99a5f039d4d
nsrau <new.urbanetz@gmail.com>
2020-10-20 23:23:50 +0200

    test: ensure RemoteAddAccount call http with correct body

--- src/data/usecases/add-account/add-account.spec.ts
+++ src/data/usecases/add-account/add-account.spec.ts
@@ -28,4 +28,11 @@ describe('RemoteAuthentication', () => {
         await sut.add(mockAddAccountParams())
         expect(httpPostClientSpy.url).toBe(url)
     })
+
+    test('Should call HttpPostClient with correct body', async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        const addAccountParams = mockAddAccountParams()
+        await sut.add(addAccountParams)
+        expect(httpPostClientSpy.body).toEqual(addAccountParams)
+    })
 })
