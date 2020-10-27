73c2288dc96dcfbf7960462bd485d731a973bac9
nsrau <new.urbanetz@gmail.com>
2020-10-27 22:01:23 +0100

    test: ensure RemoteAddAccount throws EmailInUseError if httppostclient returns 403

---
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -0,0 +1,49 @@
+import { HttpStatusCode } from '@/data/protocols/http';
+import { RemoteAddAccount } from "@/data/usecases/add-account/remote-add-account"
+import { AccountModel } from '@/domain/models'
+import { HttpPostClientSpy } from "@/data/test"
+import { AddAccountParams } from '@/domain/usecases'
+import { mockAddAccountParams } from "@/domain/test"
+import { EmailInUseError } from '@/domain/errors';
+import faker from 'faker'
+
+type SutTypes = {
+    sut: RemoteAddAccount
+    httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+    const httpPostClientSpy = new HttpPostClientSpy<AddAccountParams, AccountModel>()
+    // system under test
+    const sut = new RemoteAddAccount(url, httpPostClientSpy)
+
+    return {
+        sut,
+        httpPostClientSpy
+    }
+}
+
+describe('RemoteAuthentication', () => {
+    test('Should call HttpPostClient with correct URL', async () => {
+        const url = faker.internet.url()
+        const { sut, httpPostClientSpy } = makeSut(url)
+        await sut.add(mockAddAccountParams())
+        expect(httpPostClientSpy.url).toBe(url)
+    })
+
+    test('Should call HttpPostClient with correct body', async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        const addAccountParams = mockAddAccountParams()
+        await sut.add(addAccountParams)
+        expect(httpPostClientSpy.body).toEqual(addAccountParams)
+    })
+
+    test('Should throw EmailInUseError if HttpPostClient returns 403', async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.forbidden
+        }
+        const promise = sut.add(mockAddAccountParams())
+        await expect(promise).rejects.toThrow(new EmailInUseError())
+    })
+})
