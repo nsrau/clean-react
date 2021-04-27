6f86a34c7e938f69a0debcf14d1327d20ea53ab1
Newton Urbanetz <new.urbanetz@gmail.com>
2021-04-27 23:40:50 +0200

    test: ensure show valid status if validation succeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -63,4 +63,28 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'password')
     Helper.testStatusForField(sut, 'password', validationError)
   })
+
+  test('Should show valid name state if Validationn succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'name')
+    Helper.testStatusForField(sut, 'name')
+  })
+
+  test('Should show valid email state if Validationn succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'email')
+    Helper.testStatusForField(sut, 'email')
+  })
+
+  test('Should show valid password state if Validationn succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'password')
+    Helper.testStatusForField(sut, 'password')
+  })
+
+  test('Should show valid passwordConfirmation state if Validationn succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'passwordConfirmation')
+    Helper.testStatusForField(sut, 'passwordConfirmation')
+  })
 })
