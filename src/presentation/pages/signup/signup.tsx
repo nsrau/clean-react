1abddf3ac2bdbcfe54b0f5f308d94b30b88a8805
Newton Urbanetz <new.urbanetz@gmail.com>
2022-04-06 21:34:29 +0200

    feat: ensure SignUp calls SaveAccessToken on success

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -3,14 +3,22 @@ import Styles from './signup-styles.css'
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
-import { AddAccount } from '@/domain/usecases'
+import { AddAccount, SaveAccessToken } from '@/domain/usecases'
+import { useHistory } from 'react-router-dom'
 
 type Props = {
   validation: Validation
   addAccount: AddAccount
+  saveAccessToken: SaveAccessToken
 }
 
-const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
+const SignUp: React.FC<Props> = ({
+  validation,
+  addAccount,
+  saveAccessToken
+}: Props) => {
+  const history = useHistory()
+
   const [state, setState] = useState({
     isLoading: false,
     name: '',
@@ -44,12 +52,14 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
         ...state,
         isLoading: true
       })
-      await addAccount.add({
+      const account = await addAccount.add({
         name: state.name,
         email: state.email,
         password: state.password,
         passwordConfirmation: state.passwordConfirmation
       })
+      await saveAccessToken.save(account.accessToken)
+      history.replace('/')
     } catch (error) {
       setState({
         ...state,
@@ -61,25 +71,28 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
 
   return (
     <div className={Styles.signup}>
-      <LoginHeader />
-      <Context.Provider value={{ state, setState }}>
+      <LoginHeader/>
+      <Context.Provider value={{
+        state,
+        setState
+      }}>
         <form className={Styles.form} data-testid="form" onSubmit={handleSubmit}>
           <h2>Create account</h2>
-          <Input type="text" name="name" placeholder="insert your name" />
-          <Input type="email" name="email" placeholder="insert your email" />
-          <Input type="password" name="password" placeholder="insert your password" />
-          <Input type="password" name="passwordConfirmation" placeholder="repeat your password" />
+          <Input type="text" name="name" placeholder="insert your name"/>
+          <Input type="email" name="email" placeholder="insert your email"/>
+          <Input type="password" name="password" placeholder="insert your password"/>
+          <Input type="password" name="passwordConfirmation" placeholder="repeat your password"/>
           <button data-testid="submit"
-            disabled={!!state.emailError || !!state.passwordError || !!state.nameError || !!state.passwordConfirmationError}
-            className={Styles.submit} type="submit"
+                  disabled={!!state.emailError || !!state.passwordError || !!state.nameError || !!state.passwordConfirmationError}
+                  className={Styles.submit} type="submit"
           >
             Submit
-              </button>
+          </button>
           <span className={Styles.link}>Back login</span>
-          <FormStatus />
+          <FormStatus/>
         </form>
       </Context.Provider>
-      <Footer />
+      <Footer/>
     </div>
   )
 }
